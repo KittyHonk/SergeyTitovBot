f648d03cd987c01e84626e291eb9e32ad71012b6
KittyHonk <102020702+KittyHonk@users.noreply.github.com>
2022-04-08 02:38:39 +0300

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -1,11 +1,9 @@
 import telebot
 import sql_work
-import datetime
 from datetime import datetime
 from telebot import types
 #Токен бота
-bot = telebot.TeleBot('Вставить токен сюда')
-
+bot = telebot.TeleBot('5146742291:AAGidSkD4oPhVmlEhOfDgIVI-Ls__gYVtWU')
 
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -18,23 +16,42 @@ def start(message):
     keyboard.add(button_work_off)
     keyboard.add(button_geo)
     bot.send_message(message.chat.id, "Выбери опцию", reply_markup=keyboard)
-
+    
+@bot.message_handler(commands=["resetalreadycheck"])
+def resetalreadycheck(message):
+    #Экстренное обнуление столбца AlreadyCheck
+    sql_work.resetalreadycheck()
+    bot.send_message(message.chat.id, "Сброшено")
+    start(message)
 
 @bot.message_handler(content_types=['text', 'location'])
 def work_option(message):
+    curWork, check = sql_work.check_for_worknow(message.from_user.id)
+    if curWork == 0:
+        worknow = False
+    else:
+        worknow = True
+    print(worknow, check)
     #Логика выхода/ухода с работы
     if message.text == "Пришел на работу":
-        bot.send_message(message.chat.id, "Рабочий день начался, отправь геолокацию")
-        bot.register_next_step_handler(message, location);
+        if worknow is False and check is False:
+            bot.send_message(message.chat.id, "Рабочий день начался, отправь геолокацию")
+            bot.register_next_step_handler(message, locationIn);
+        elif worknow is True and check is True: 
+            bot.send_message(message.chat.id, "Рабочий день уже начат")
+        elif worknow is False and check is True:
+            bot.send_message(message.chat.id, "Сегодня уже отмечался")
     elif message.text == "Ушел с работы":
-        bot.send_message(message.chat.id, "Рабочий день закончился, отправь геолокацию")  
-        bot.register_next_step_handler(message, location);
+        if worknow is True:
+            bot.send_message(message.chat.id, "Рабочий день закончился, отправь геолокацию")  
+            bot.register_next_step_handler(message, locationOut);
+        else:
+            bot.send_message(message.chat.id, "Рабочий день уже закончен / не начат")
     else: 
         bot.send_message(message.chat.id, "Неизвестная команда, попробуй еще раз")
         start(message)
-    
-    
-def location(message):
+
+def locationIn(message):
     #Логика снятия геолокации + проверка на forward
     if message.location is not None and message.forward_from is None:
         bot.send_message(message.chat.id, "Геолокация получена")
@@ -43,9 +60,21 @@ def location(message):
     else:
         bot.send_message(message.chat.id, "Геолокация не получена, начни/заверши рабочий день еще раз")
         start(message)
+        
+def locationOut(message):
+    #Тоже самое что и locationIn + вычисление времени дельты времени работы
+    if message.location is not None and message.forward_from is None:
+        bot.send_message(message.chat.id, "Геолокация получена")
+        sql_work.update_in_db(message.from_user.id, message.location.longitude, message.location.latitude)
+        sql_work.difftime(message.from_user.id)
+    else:
+        bot.send_message(message.chat.id, "Геолокация не получена, начни/заверши рабочий день еще раз")
+        start(message)
+
 
 #Запросы на изменения от бота, можно настроить interval, в случае ошибки пропускает тик запроса
-try: 
-    bot.polling(none_stop=True, interval=0)
-except Exception:
-    pass
+if __name__ == '__main__':
+    try:
+        bot.polling()
+    except Exception:
+        pass
--- sql_work.py
+++ sql_work.py
@@ -2,7 +2,6 @@ import pymysql
 import uuid
 from datetime import datetime
 from pymysql.cursors import DictCursor
-from contextlib import closing
 
 #Тут настраивается подключение к БД
 connection = pymysql.connect(
@@ -17,7 +16,27 @@ connection = pymysql.connect(
 def create_table():
     #Создание таблицы в случае её отсутствия
     with connection.cursor() as cursor:      
-        query = "CREATE TABLE IF NOT EXISTS Worker (RowId varchar(255), UserId int, LastName varchar(255), FirstName varchar(255), Longitude float(6), Latitude float(6), CurDateTime DATETIME, UNIQUE KEY(RowId))"
+        query = "CREATE TABLE IF NOT EXISTS WorkerNew (RowId varchar(255), UserId int, LastName varchar(255), FirstName varchar(255), LongitudeIn float(6), LatitudeIn float(6), DateTimeIn DATETIME, LongitudeOut float(6), LatitudeOut float(6), DateTimeOut DATETIME, DiffTime varchar(255), CurWork int, AlreadyCheck int, UNIQUE KEY(RowId))"
+        cursor.execute(query)
+        connection.commit()
+        
+def create_scheduler():
+    #Создание регулярной очистки ночью столбца AlreadyCheck через Schedule MySQL
+    with connection.cursor() as cursor:
+        query = "set global event_scheduler = on"
+        cursor.execute(query)
+        connection.commit()
+        query = "CREATE EVENT new_day ON SCHEDULE EVERY 1 DAY STARTS '2022-04-06 21:00:01.000' ON COMPLETION NOT PRESERVE ENABLE DO update WorkerNew set AlreadyCheck = 0 where AlreadyCheck = 1"
+        try:
+            cursor.execute(query)
+        except:
+            pass
+        connection.commit()
+        
+def resetalreadycheck():
+    #Функция сброса AlreadyCheck
+    with connection.cursor() as cursor:
+        query = "UPDATE WorkerNew set AlreadyCheck = 0 WHERE AlreadyCheck = 1"
         cursor.execute(query)
         connection.commit()
 
@@ -25,17 +44,66 @@ def insert_in_db(id, last_name, first_name):
     #Вставка в БД имени, фамилии и айди пользователя + присвоение уникального ключа
     with connection.cursor() as cursor:
         unique_key = str(uuid.uuid4())
-        query = "INSERT INTO Worker (RowId, UserId, LastName, FirstName, CurDateTime) VALUES" + "('{}', {}, '{}', '{}', '{}')".format(unique_key, id, last_name, first_name, datetime.today())
+        query = "INSERT INTO WorkerNew (RowId, UserId, LastName, FirstName, DateTimeIn, CurWork, AlreadyCheck) VALUES" + "('{}', {}, '{}', '{}', '{}', {}, {})".format(unique_key, id, last_name, first_name, datetime.today(), 1, 1)
         cursor.execute(query)
         connection.commit()
         return unique_key
-        
+
 def insert_geo(unique_key, longitude, latitude):
     #Вставка в таблицу геолокации
     with connection.cursor() as cursor:      
-        query = "UPDATE Worker SET Longitude = {}, Latitude = {} WHERE RowId =".format(longitude, latitude) + "'{}'".format(unique_key)
+        query = "UPDATE WorkerNew SET LongitudeIn = {}, LatitudeIn = {} WHERE RowId =".format(longitude, latitude) + "'{}'".format(unique_key)
         cursor.execute(query)
         connection.commit()
 
+def update_in_db(userid, longitude, latitude):
+    #Обновление информации о пользователе
+    with connection.cursor() as cursor:
+        query = "UPDATE WorkerNew SET LongitudeOut = {}, LatitudeOut = {}, ".format(longitude, latitude) + "DateTimeOut = " + "'{}' ".format(datetime.today()) + "WHERE UserId = '{}' and CurWork = 1".format(userid)
+        cursor.execute(query)
+        connection.commit()
+
+def check_for_worknow(userid):
+    #Проверка на состояние работника
+    with connection.cursor() as cursor:
+        curWork = None
+        alreadyCheck = None
+        query = "SELECT * FROM WorkerNew WHERE CurWork = 1 and UserId = {}".format(userid)
+        curWork = cursor.execute(query)
+        query = "SELECT AlreadyCheck FROM WorkerNew WHERE AlreadyCheck = 1 and UserId = {}".format(userid)
+        cursor.execute(query)
+        alreadyCheck = cursor.fetchall()
+        print(alreadyCheck)
+        check = False
+        for i in range(len(alreadyCheck)):
+            if alreadyCheck[i]['AlreadyCheck'] == 1:
+                check = True
+        connection.commit()
+        return curWork, check
+
+def difftime(userid):
+    #Вычисление отработаного времени
+    with connection.cursor() as cursor:
+        query = "SELECT DateTimeIn, DateTimeOut FROM WorkerNew WHERE CurWork = 1 and UserId = " + "'{}'".format(userid)
+        cursor.execute(query)
+        worktime = cursor.fetchall()
+        for i in range(len(worktime)):
+            w1 = worktime[i]['DateTimeIn']
+            w2 = worktime[i]['DateTimeOut']
+            tdelta = w2 - w1
+            query = "UPDATE WorkerNew SET CurWork = 0, DiffTime = " + "'{}'".format(tdelta) + " WHERE CurWork = 1 and UserId = " + "{}".format(userid)
+            cursor.execute(query)
+            connection.commit()
+
+def new_day():
+    #Обнуление столбца AlreadyCheck отвечающего за возможность отметки за текущий день
+    with connection.cursor() as cursor:
+        query = "UPDATE WorkerNew SET AlreadyCheck = 0"
+        cursor.execute(query)
+        connection.commit()
+        print('AlreadyCheck is null')
+        
 #Вызов метода для создания таблицы
 create_table()
+create_scheduler()
+
