2de7ab88c35797b10b4d301bb94cb560716d8255
KittyHonk <102020702+KittyHonk@users.noreply.github.com>
2022-05-25 22:37:10 +0300

    Add files via upload

--- bot_main.py
+++ bot_main.py
@@ -1,3 +1,4 @@
+from click import command
 import telebot
 import sql_work
 from datetime import datetime
@@ -12,18 +13,22 @@ def start(message):
     button_work_on = types.KeyboardButton(text="Пришел на работу")
     button_work_off = types.KeyboardButton(text="Ушел с работы")
     button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    button_processing = types.KeyboardButton(text="Учет переработок")
+    button_statistic = types.KeyboardButton(text="Статистика за последний месяц")
     keyboard.add(button_work_on)
     keyboard.add(button_work_off)
     keyboard.add(button_geo)
+    keyboard.add(button_processing)
+    keyboard.add(button_statistic)
     bot.send_message(message.chat.id, "Выбери опцию", reply_markup=keyboard)
-    
+
 @bot.message_handler(commands=["resetalreadycheck"])
 def resetalreadycheck(message):
     #Экстренное обнуление столбца AlreadyCheck
     sql_work.resetalreadycheck()
     bot.send_message(message.chat.id, "Сброшено")
     start(message)
-
+    
 @bot.message_handler(content_types=['text', 'location'])
 def work_option(message):
     curWork, check = sql_work.check_for_worknow(message.from_user.id)
@@ -31,7 +36,7 @@ def work_option(message):
         worknow = False
     else:
         worknow = True
-    #Логика выхода/ухода с работы
+    #Логика выхода/ухода с работы + учет времени
     if message.text == "Пришел на работу":
         if worknow is False and check is False:
             bot.send_message(message.chat.id, "Рабочий день начался, отправь геолокацию")
@@ -46,6 +51,26 @@ def work_option(message):
             bot.register_next_step_handler(message, locationOut);
         else:
             bot.send_message(message.chat.id, "Рабочий день уже закончен / не начат")
+    elif message.text == "Учет переработок":
+        processing(message)
+    elif message.text == "Вернуться":
+        start(message)
+    elif message.text == "Взять выходной":
+        reply = sql_work.get_holiday(message.from_user.id)
+        bot.send_message(message.chat.id, "{}".format(reply))
+        start(message)
+    elif message.text == "Статистика за последний месяц":
+        reply = sql_work.get_statistic()
+        for i in range(len(reply)):
+            hours = reply[i]['sum(DiffTime)']
+            day = 0
+            while (hours >= 24):
+                day += 1
+                hours -= 24
+            bot.send_message(message.chat.id, "Фамилия: {}\nИмя: {}\nДней: {}\nЧасов: {}\n".format(
+                reply[i]['LastName'], reply[i]['FirstName'], day, hours
+                ))
+        start(message)
     else: 
         bot.send_message(message.chat.id, "Неизвестная команда, попробуй еще раз")
         start(message)
@@ -65,11 +90,22 @@ def locationOut(message):
     if message.location is not None and message.forward_from is None:
         bot.send_message(message.chat.id, "Геолокация получена")
         sql_work.update_in_db(message.from_user.id, message.location.longitude, message.location.latitude)
-        sql_work.difftime(message.from_user.id)
+        tdelta = sql_work.difftime(message.from_user.id)
+        sql_work.insert_in_processing_db(message.from_user.id, message.from_user.last_name, message.from_user.first_name, tdelta)
     else:
         bot.send_message(message.chat.id, "Геолокация не получена, начни/заверши рабочий день еще раз")
         start(message)
 
+def processing(message):
+    #Вывод количества часов переработок
+    processing_time = sql_work.check_processing_time(message.from_user.id)
+    bot.send_message(message.chat.id, "На текущий момент часов на балансе: {}".format(processing_time[0]['DateTimeProcessing']))
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_work_on = types.KeyboardButton(text="Взять выходной")
+    button_work_off = types.KeyboardButton(text="Вернуться")
+    keyboard.add(button_work_on)
+    keyboard.add(button_work_off)
+    bot.send_message(message.chat.id, "Выбери опцию", reply_markup=keyboard)
 
 #Запросы на изменения от бота, можно настроить interval, в случае ошибки пропускает тик запроса
 if __name__ == '__main__':
--- sql_work.py
+++ sql_work.py
@@ -1,6 +1,9 @@
+from concurrent.futures import process
+from sqlite3 import connect
 import pymysql
 import uuid
 from datetime import datetime
+from datetime import timedelta
 from pymysql.cursors import DictCursor
 
 #Тут настраивается подключение к БД
@@ -8,7 +11,7 @@ connection = pymysql.connect(
     host='localhost', #Хост БД
     user='root', #Пользователь, требуется с доступом на изменение таблиц
     password='admin', #Пароль пользователя
-    db='pyTest', #БД для подключения
+    db='pytest', #БД для подключения
     charset='utf8mb4', #Кодировка БД, стоит по умолчанию для MySQL
     cursorclass=DictCursor
 )   
@@ -17,6 +20,13 @@ def create_table():
     #Создание таблицы в случае её отсутствия
     with connection.cursor() as cursor:      
         query = "CREATE TABLE IF NOT EXISTS WorkerNew (RowId varchar(255), UserId int, LastName varchar(255), FirstName varchar(255), LongitudeIn float(6), LatitudeIn float(6), DateTimeIn DATETIME, LongitudeOut float(6), LatitudeOut float(6), DateTimeOut DATETIME, DiffTime varchar(255), CurWork int, AlreadyCheck int, UNIQUE KEY(RowId))"
+        cursor.execute(query)
+        connection.commit()
+        
+def create_table_timeCheck():
+    #Создание таблицы учета времени переработок
+    with connection.cursor() as cursor:      
+        query = "CREATE TABLE IF NOT EXISTS Processing (RowId varchar(255), UserId int, LastName varchar(255), FirstName varchar(255), DateTimeProcessing float(6), UNIQUE KEY(RowId))"
         cursor.execute(query)
         connection.commit()
         
@@ -39,6 +49,50 @@ def resetalreadycheck():
         query = "UPDATE WorkerNew set AlreadyCheck = 0 WHERE AlreadyCheck = 1"
         cursor.execute(query)
         connection.commit()
+        
+def insert_in_processing_db(id, last_name, first_name, processing_time):
+    #Вставка в БД по учету переработок: имени, фамилии, айди, в случае отсутсвия - создание такой строки
+    with connection.cursor() as cursor:
+        unique_key = str(uuid.uuid4())
+        query = "SELECT * FROM Processing WHERE UserId = {}".format(id)
+        cursor.execute(query)
+        check = cursor.fetchall()
+        if (check == ()):
+            query = "INSERT INTO Processing (RowId, UserId, LastName, FirstName) VALUES" + "('{}', {}, '{}', '{}')".format(unique_key, id, last_name, first_name)
+            cursor.execute(query)
+            connection.commit()
+        delta = processing_time
+        minutes = round(delta.seconds/60, 2)
+        hours = round(minutes/60, 2)
+        if hours >= 8:
+            hours -= 8;
+            query = "UPDATE Processing SET DateTimeProcessing = {} WHERE UserId = {}".format(hours, id)
+            cursor.execute(query)
+            connection.commit()
+        connection.commit()
+
+def check_processing_time(id):
+    with connection.cursor() as cursor:
+        query = "SELECT DateTimeProcessing FROM Processing WHERE UserId = {}".format(id)
+        cursor.execute(query)
+        check = cursor.fetchall()
+        return check
+
+def get_holiday(id):
+    with connection.cursor() as cursor:
+        query = "SELECT DateTimeProcessing FROM Processing WHERE UserId = {}".format(id)
+        cursor.execute(query)
+        check = cursor.fetchall()
+        check = check[0]['DateTimeProcessing']
+        if (check >= 8):
+            check -= 8
+            query = "UPDATE Processing SET DateTimeProcessing = {} WHERE UserId = {}".format(check, id)
+            cursor.execute(query)
+            connection.commit()
+            return "Выходной взят"
+        else:
+            return "Выходной не взят, недостаточно часов на балансе"
+        
 
 def insert_in_db(id, last_name, first_name):
     #Вставка в БД имени, фамилии и айди пользователя + присвоение уникального ключа
@@ -73,7 +127,6 @@ def check_for_worknow(userid):
         query = "SELECT AlreadyCheck FROM WorkerNew WHERE AlreadyCheck = 1 and UserId = {}".format(userid)
         cursor.execute(query)
         alreadyCheck = cursor.fetchall()
-        print(alreadyCheck)
         check = False
         for i in range(len(alreadyCheck)):
             if alreadyCheck[i]['AlreadyCheck'] == 1:
@@ -87,13 +140,35 @@ def difftime(userid):
         query = "SELECT DateTimeIn, DateTimeOut FROM WorkerNew WHERE CurWork = 1 and UserId = " + "'{}'".format(userid)
         cursor.execute(query)
         worktime = cursor.fetchall()
-        for i in range(len(worktime)):
-            w1 = worktime[i]['DateTimeIn']
-            w2 = worktime[i]['DateTimeOut']
-            tdelta = w2 - w1
-            query = "UPDATE WorkerNew SET CurWork = 0, DiffTime = " + "'{}'".format(tdelta) + " WHERE CurWork = 1 and UserId = " + "{}".format(userid)
-            cursor.execute(query)
-            connection.commit()
+        w1 = worktime[0]['DateTimeIn']
+        w2 = worktime[0]['DateTimeOut']
+        tdelta = w2 - w1
+        minutes = round(tdelta.seconds/60, 2)
+        hours = round(minutes/60, 2)
+        query = "UPDATE WorkerNew SET CurWork = 0, DiffTime = " + "'{}'".format(hours) + " WHERE CurWork = 1 and UserId = " + "{}".format(userid)
+        cursor.execute(query)
+        connection.commit()
+        return tdelta
+    
+def get_statistic():
+    #Вывод общей статистики по отработаному времени
+    with connection.cursor() as cursor:
+        today = datetime.today()
+        year = today.year - 3
+        month = today.month
+        day = today.day
+        delta = monthdelta(datetime(year, month, day), -1)
+        query = "SELECT LastName, FirstName, sum(DiffTime) FROM WorkerNew WHERE DateTimeIn >= " + "'{}'".format(delta) + " GROUP BY FirstName"
+        cursor.execute(query)
+        return (cursor.fetchall())
+        
+def monthdelta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1)
+    if not m: m = 12
+    d = min(date.day, [31,
+        29 if y%4==0 and (not y%100==0 or y%400 == 0) else 28,
+        31,30,31,30,31,31,30,31,30,31][m-1])
+    return date.replace(day=d,month=m, year=y)    
 
 def new_day():
     #Обнуление столбца AlreadyCheck отвечающего за возможность отметки за текущий день
@@ -101,9 +176,9 @@ def new_day():
         query = "UPDATE WorkerNew SET AlreadyCheck = 0"
         cursor.execute(query)
         connection.commit()
-        print('AlreadyCheck is null')
         
 #Вызов метода для создания таблицы
 create_table()
+create_table_timeCheck()
 create_scheduler()
-
+get_statistic()
